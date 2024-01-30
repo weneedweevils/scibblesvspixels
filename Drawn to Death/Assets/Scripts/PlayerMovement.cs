38dafb2eb59e24940d44c54226ecb7b449746efd
Derek Frevel <derekf711@gmail.com>
2024-01-30 15:52:02 -0700

    Player Movement
    - Added simple player movement
    - Missing collisions

---
+++ Drawn to Death/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,35 @@
+﻿/*  References:
+ *      - Youtube: Creating 2D Player Movement for Isometric Games with Unity 2018.3! (Tutorial)
+ *          - By: Unity
+ *          - Link: https://www.youtube.com/watch?v=tywt9tOubEY
+ *          
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 1.0f;
+    Rigidbody2D rbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 currentPos = rbody.position;
+        float hInput = Input.GetAxis("Horizontal");
+        float vInput = Input.GetAxis("Vertical");
+        Vector2 inputVector = new Vector2(hInput, vInput);
+        inputVector = Vector2.ClampMagnitude(inputVector, 1);
+        Vector2 movement = inputVector * moveSpeed;
+        Vector2 newPos = currentPos + movement * Time.fixedDeltaTime;
+        rbody.MovePosition(newPos);
+    }
+}
