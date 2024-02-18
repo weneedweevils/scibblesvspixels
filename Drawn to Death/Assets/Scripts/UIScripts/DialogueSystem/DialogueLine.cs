8a331e8e0f15659fe9e8ea52c2571ae2a92ecab9
Ryan Haskins <rhaskins@ualberta.ca>
2024-02-18 11:58:22 -0700

    Added Dialogue System

---
+++ Drawn to Death/Assets/Scripts/UIScripts/DialogueSystem/DialogueLine.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace DialogueSystem
+{
+    public class DialogueLine : DialogueBaseClass
+    {
+        private Text textHolder;
+
+        [Header ("Text Customizers")]
+        [SerializeField] private string input;
+        [SerializeField] private Color textColor;
+        [SerializeField] private Font textFont;
+
+        [Header("Time Variables")]
+        [SerializeField] private float delay;
+
+        [Header("Character Image")]
+        [SerializeField] private Sprite characterSprite;
+        [SerializeField] private Image imageHolder;
+
+
+        private void Awake()
+        {
+            textHolder = GetComponent<Text>();
+            textHolder.text = "";
+            imageHolder.sprite = characterSprite;
+            imageHolder.preserveAspect = true;
+        }
+
+        private void Start()
+        {
+            StartCoroutine(WriteText(input, textHolder, textColor, textFont, delay));
+        }
+    }
+}
