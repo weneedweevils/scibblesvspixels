21f60092b2bedf72cc5667b381938b89e04c641e
Derek Frevel <derekf711@gmail.com>
2024-02-28 22:39:56 -0700

    Added end scene and trigger

--- Drawn to Death/Assets/Scripts/UIScripts/MenuManager.cs
+++ Drawn to Death/Assets/Scripts/UIScripts/MenuManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public enum Scene { Menu, Intructions, Level_1, Ded}
+public enum Scene { Menu, Intructions, Level_1, Ded, End}
 public class MenuManager : MonoBehaviour
 {
     public Scene nextScene;
