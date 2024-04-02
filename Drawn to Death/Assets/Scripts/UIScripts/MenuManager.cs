c3eea5e1518c4981482e27948715bee2dd0ebbee
Gmaliakal <77520861+Gmaliakal@users.noreply.github.com>
2024-04-01 18:40:39 -0600

    Updated Menu scene

--- Drawn to Death/Assets/Scripts/UIScripts/MenuManager.cs
+++ Drawn to Death/Assets/Scripts/UIScripts/MenuManager.cs
@@ -1,15 +1,21 @@
 ﻿ using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public enum Scene { Menu, Intructions, Level_1, Level_2, Level_3, Ded, End}
 public class MenuManager : MonoBehaviour, IDataPersistence
 {
+    public TextMeshProUGUI text;
+    public GameObject Button;
     public Scene nextScene;
     public bool newGame = false;
     public bool loadGame = false;
+ 
 
+  
     public void QuitGame(){
         Application.Quit();
     }
@@ -41,5 +47,15 @@ public class MenuManager : MonoBehaviour, IDataPersistence
     public void SaveData(ref GameData data)
     {
         return;
+    }
+
+    public void OnHovered()
+    {
+        text.color = new Color(255,255,255,0.50f);
+    }
+
+    public void OnHoveredExit()
+    {
+        text.color = new Color(255, 255, 255, 1f);
     }
 }
