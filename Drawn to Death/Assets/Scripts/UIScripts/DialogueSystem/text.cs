4c34ad7907f3bbbd1c870e5f0338e5d4fe56ae97
Gmaliakal <77520861+Gmaliakal@users.noreply.github.com>
2024-04-08 16:07:48 -0600

    bug fixes and made bones bouncier

--- Drawn to Death/Assets/Scripts/UIScripts/DialogueSystem/text.cs
+++ Drawn to Death/Assets/Scripts/UIScripts/DialogueSystem/text.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using System.IO;
 using TMPro;
 using UnityEngine.TextCore;
+using UnityEngine.XR;
 
 public class text : MonoBehaviour
 {
@@ -16,23 +17,11 @@ public class text : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-       Phrases.Add("BAHAHAHAHAHAHAHAHHAHAHAHAH");
+       Phrases.Add("Rattled yer bones, eh spooky?");
+       Phrases.Add("Looks like ya need a hand.Oh wait");
+       Phrases.Add("Skill Issue");
        input = Phrases[Random.Range(0,Phrases.Count-1)];
        textMeshPro.text = input;
-    }
-
-    // got from here https://gamedev.stackexchange.com/questions/85807/how-to-read-a-data-from-text-file-in-unity
-    void readTextFile(string file_path)
-    {
-        StreamReader inp_stm = new StreamReader(file_path);
-
-        while (!inp_stm.EndOfStream)
-        {
-            string inp_ln = inp_stm.ReadLine();
-            // Do Something with the input. 
-        }
-
-        inp_stm.Close();
     }
 
 }
