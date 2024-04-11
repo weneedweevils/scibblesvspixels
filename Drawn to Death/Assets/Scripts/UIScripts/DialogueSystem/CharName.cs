d7cc1a80fdcb40f0bb4ab6df10b31051a7736c11
Ryan Haskins <rhaskins@ualberta.ca>
2024-04-10 22:13:51 -0600

    Added name section in dialogue boxes and Oodler animations

---
+++ Drawn to Death/Assets/Scripts/UIScripts/DialogueSystem/CharName.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharName : MonoBehaviour
+{
+    private Text textHolder;
+
+    [Header("Text Customizers")]
+    [SerializeField] private string input;
+    [SerializeField] private Color textColor;
+    [SerializeField] private Font textFont;
+
+    // Ensures line runs and gets reset properly if text gets repeated
+    private void OnEnable()
+    {
+        ResetLine();
+        textHolder.color = textColor;
+        textHolder.font = textFont;
+        textHolder.text = input;
+    }
+
+    // Resets line so it can be repeated if nessecary
+    private void ResetLine()
+    {
+        textHolder = transform.GetChild(0).gameObject.GetComponent<Text>();
+        textHolder.text = "";
+    }
+}
