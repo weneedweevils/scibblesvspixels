7b88350500464688ede162096eaddc190a1814c2
Gerrinator <vangenderengerard@gmail.com>
2024-08-25 00:29:47 -0600

    Finished overall level layout

--- Drawn to Death/Assets/Scripts/ConditionalWall.cs
+++ Drawn to Death/Assets/Scripts/ConditionalWall.cs
@@ -2,8 +2,46 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum TriggerTypes {EnemiesKilled, Collision};
 public class ConditionalWall : MonoBehaviour
 {
-    public TriggerTypes triggerType;
+    public EnemyAI[] enemies;
+    public Sprite destroyedSprite;
+    
+    private bool allDead;
+    private bool destroyed;
+    private SpriteRenderer spriteRenderer;
+    private PolygonCollider2D polygonCollider2D;
+
+    public GameObject explosion;
+
+    public void Start(){
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        polygonCollider2D = GetComponent<PolygonCollider2D>();
+        destroyed = false;
+    }
+    public void Update(){
+        allDead = true;
+
+        if (!destroyed){
+            // Check if all enemies are dead
+            foreach (EnemyAI enemy in enemies){
+                if (!(enemy.isDead() || enemy.team == 0)){
+                    allDead = false;
+                    break;
+                }
+            }
+
+            if (allDead){
+                ConditionReached();
+                destroyed = true;
+            }
+        }
+    }
+
+    public void ConditionReached(){
+        spriteRenderer.sprite = destroyedSprite;
+        polygonCollider2D.enabled = false;
+        Instantiate(explosion, transform.position, Quaternion.identity);
+        FMODUnity.RuntimeManager.PlayOneShot("event:/CellWallBreak", this.transform.position);
+    }
 }
