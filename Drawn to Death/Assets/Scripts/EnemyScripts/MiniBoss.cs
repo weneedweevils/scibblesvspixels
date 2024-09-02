ad8680cd3671161ba400274e5bc0ea5d3c4b3914
Derek Frevel <derekf711@gmail.com>
2024-09-02 17:39:39 -0600

    Added special ability to Mini boss
    - Added a new 'Boost' ability to the mini boss where the speed of the Mini boss will dramatically increase for a short period of time
      - Speed increases by 7x
      - Boost lasts for 0.5 seconds
      - Boost cool down is 4 seconds

--- Drawn to Death/Assets/Scripts/EnemyScripts/MiniBoss.cs
+++ Drawn to Death/Assets/Scripts/EnemyScripts/MiniBoss.cs
@@ -7,19 +7,50 @@ public class MiniBoss : OodleKnight
 {
     public GameObject bossHPBar;
     public TMPro.TextMeshProUGUI hpInfo;
+    public float boostSpeedModifier;
+    public float boostDuration;
+    public float boostCooldown;
+    [HideInInspector] public float baseSpeed;
+    public CooldownTimer boostTimer;
+
+    override protected void Start()
+    {
+        base.Start();
+        baseSpeed = speed;
+        boostTimer = new CooldownTimer(boostCooldown, boostDuration);
+    }
 
     override protected void FixedUpdate()
     {
         base.FixedUpdate();
-        
-        if (state == State.chase || state == State.attack)
+
+        if (!playerMovement.inFreezeDialogue() && !playerMovement.timelinePlaying)
         {
-            bossHPBar.SetActive(true);
-            hpInfo.text = string.Format("{0} / {1}", Mathf.CeilToInt(health), Mathf.CeilToInt(maxHealth));
-        }
-        else
-        {
-            bossHPBar.SetActive(false);
+            //Enable/Disable bossHPBar
+            if (state == State.chase || state == State.attack)
+            {
+                bossHPBar.SetActive(true);
+                hpInfo.text = string.Format("{0} / {1}", Mathf.CeilToInt(health), Mathf.CeilToInt(maxHealth));
+            }
+            else
+            {
+                bossHPBar.SetActive(false);
+            }
+
+            //Boost Ability
+            boostTimer.Update();
+            if (boostTimer.IsUseable())
+            {
+                boostTimer.StartTimer();
+            }
+            else if (boostTimer.IsActive())
+            {
+                speed = baseSpeed * boostSpeedModifier;
+            }
+            else if (boostTimer.IsOnCooldown())
+            {
+                speed = baseSpeed;
+            }
         }
     }
 
