a212341ffa04d38d19f535476c6045cdb07db17f
Derek Frevel <derekf711@gmail.com>
2024-10-09 11:59:45 -0600

    Created Demo end trigger script
    Created a simple script that will changes scenes when you kill a specific set of doodle bars

---
+++ Drawn to Death/Assets/DemoFeatures.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DemoFeatures : MonoBehaviour
+{
+    public DoodleBars bars;
+    public MenuManager menu;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (bars.isDead())
+        {
+            menu.GotoScene();
+        }
+    }
+}
