74f4b1c00b86fabe7d31240abc7ecfb053849263
RTHaskins <ryan.haskins12@gmail.com>
2024-12-20 20:31:29 -0700

    Reorganized files :)

---
+++ Drawn to Death/Assets/Scripts/MiscScripts/SnapToGrid.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnapToGrid : MonoBehaviour
+{
+    public Grid grid;
+    public Vector3 offset;
+
+    private void OnDrawGizmosSelected()
+    {
+        transform.position = Snap(transform.position - offset, grid) + offset;
+    }
+
+    public static Vector3 Snap(Vector3 pos, Grid grid)
+    {
+        return grid.CellToWorld(grid.WorldToCell(pos));
+    }
+}
