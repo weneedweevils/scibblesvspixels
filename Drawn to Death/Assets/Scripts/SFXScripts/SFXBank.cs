ab4f6ca0429c677dff131e4d4d9d6b9b729ecb38
Derek Frevel <derekf711@gmail.com>
2025-05-23 14:25:14 -0600

    Beautified Display Settings

---
+++ Drawn to Death/Assets/Scripts/SFXScripts/SFXBank.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "SFX Bank", menuName = "SFX Bank")]
+public class SFXBank : ScriptableObject
+{
+    public FMODUnity.EventReference[] SoundEffects;
+
+    public void PlaySFX(int id)
+    {
+        if(id >= 0 && id < SoundEffects.Length)
+        {
+            FMODUnity.RuntimeManager.PlayOneShot(SoundEffects[id]);
+        }
+    }
+}
