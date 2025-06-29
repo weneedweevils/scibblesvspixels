c6187f9bee17ae92d9eef855a38ea3455054aa91
Gmaliakal <77520861+Gmaliakal@users.noreply.github.com>
2025-06-29 13:32:48 -0600

    basic speedrun timer runs in the main menu, seems to lag when testing at times past 3 hours. Need to make sure that it runs in between levels, it toggles on and off, and that it pauses during loading screens and cutscenes

---
+++ Drawn to Death/Assets/Scripts/UIScripts/SpeedRunTimer.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+using System.Diagnostics;
+using System;
+using System.Text;
+
+public class SpeedRunTimer : MonoBehaviour
+{
+    private TMPro.TextMeshProUGUI textMeshPro;
+    private float timer;
+
+
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+        textMeshPro = GetComponentInChildren<TextMeshProUGUI>();
+        timer = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (SceneManager.GetActiveScene().name == "menu" )
+        //{
+            timer += Time.deltaTime;
+            float seconds = timer % 60;
+            float minutes = Mathf.Floor(timer / 60);
+            float corrected_minutes = minutes % 60;
+            float hours = Mathf.Floor(minutes / 60);
+            textMeshPro.SetText(string.Format("{0}:{1}:{2}", hours.ToString(), corrected_minutes.ToString("00"), seconds.ToString("00.00")));
+        //}
+        
+    }
+}
