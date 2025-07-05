dd89ec7c866f21aa89a12d89692444e0aaf9bdad
Derek Frevel <derekf711@gmail.com>
2025-07-05 16:48:03 -0600

    Added support for dialogue Icons
    Still in testing phase

--- Drawn to Death/Assets/Scripts/Dialogue/Dialogue Variables/TutorialVariables.cs
+++ Drawn to Death/Assets/Scripts/Dialogue/Dialogue Variables/TutorialVariables.cs
@@ -13,10 +13,23 @@ public class TutorialVariables : MonoBehaviour
     public DialogueVariable rally;
     public DialogueVariable revive;
 
+    [Space(20)]
+    public IconMap[] iconMap;
+    public Dictionary<string, string> icons;
+
     // Start is called before the first frame update
     void Start()
     {
         playerInput = CustomInput.instance.playerInput;
+
+        icons = new Dictionary<string, string>();
+        foreach (IconMap map in iconMap)
+        {
+            if (!icons.ContainsKey(map.keyword))
+            {
+                icons.Add(map.keyword, map.iconName);
+            }
+        }
     }
 
     public void Update()
@@ -32,7 +45,23 @@ public class TutorialVariables : MonoBehaviour
     {
         if (action != null && variable != null)
         {
-            variable.text = action.GetBindingDisplayString().ToUpper();
+            variable.text = TryIcon(action.GetBindingDisplayString().ToUpper());
         }
     }
+
+    public string TryIcon(string key)
+    {
+        if (icons.ContainsKey(key))
+        {
+            return string.Format("<sprite name=\"{0}\">", icons[key]);
+        }
+        return key;
+    }
+}
+
+[System.Serializable]
+public class IconMap
+{
+    public string keyword;
+    public string iconName;
 }
