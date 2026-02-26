de4c280e0106ba12ab0455b75d43f39ea4f51206
Gmaliakal <77520861+Gmaliakal@users.noreply.github.com>
2026-02-25 18:35:23 -0700

    fixed missing boss script and commented out script from other branch

---
+++ Drawn to Death/Assets/Scripts/UIScripts/CircleKill.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircleKill : MonoBehaviour { 
+
+    CircleCollider2D circleCollider;
+    RectTransform rectTransform;
+
+    void  OnEnable()
+    {
+        circleCollider = GetComponent<CircleCollider2D>();
+        rectTransform = GetComponent<RectTransform>();
+        circleCollider.enabled = false;
+    }
+    void FixedUpdate()
+    {
+        if (circleCollider.enabled)
+        {
+            circleCollider.radius = rectTransform.rect.width / 2f;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Enemy")
+        {
+            //collision.gameObject.GetComponent<EnemyAI>().InstantKill();
+        }
+    }
+}
