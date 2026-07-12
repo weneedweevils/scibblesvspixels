533396aa01f7eb8549afa973476cdaa1dd238da3
Derek Frevel <derekf711@gmail.com>
2026-07-12 00:50:51 -0600

    Altered Oodler death sequence
    - Created dedicated manager to handle death sequence
    - Created simple explosion object with randomized properties

---
+++ Drawn to Death/Assets/Scripts/ControlScripts/OodlerDeathManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.ShaderGraph.Internal;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OodlerDeathManager : Singleton<OodlerDeathManager>
+{
+    [SerializeField] private MenuManager menuManager;
+    [SerializeField] private GameObject explosionPrefab;
+    [SerializeField] float explosionSpawnInterval = 0.5f;
+    [SerializeField] private Oodler oodler;
+    [SerializeField] private Image screenFade;
+    [SerializeField] private float fadeDuration;
+
+    private void Start()
+    {
+        oodler.OnDeath.AddListener(HandleOodlerDeath);
+    }
+
+    private void HandleOodlerDeath()
+    {
+        PlayerMovement.instance.SetTimelineActive(true);
+        oodler.enabled = false;
+
+        StartCoroutine(OodlerDeathCoroutine());
+    }
+
+    public IEnumerator OodlerDeathCoroutine()
+    {
+        int count = 0;
+
+        float elapsedTime = 0f;
+        while (elapsedTime < fadeDuration)
+        {
+            elapsedTime += Time.deltaTime;
+            float alpha = Mathf.Clamp01(elapsedTime / fadeDuration);
+            MyUtils.SetAlpha(screenFade, alpha);
+
+            if (elapsedTime >= count * explosionSpawnInterval)
+            {
+                Debug.Log("Spawning explosion at: " + oodler.transform.position + " at time: " + elapsedTime);
+                Instantiate(explosionPrefab, oodler.transform.position, Quaternion.identity);
+                count++;
+            }
+            yield return null;
+        }
+
+        MyUtils.SetAlpha(screenFade, 1f);
+        yield return new WaitForSeconds(0.5f);
+        menuManager.GotoScene();
+    }
+}
