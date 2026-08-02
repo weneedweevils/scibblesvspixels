c4d4bfd2753d517e9d48183942aa35869f0d562d
Derek Frevel <derekf711@gmail.com>
2026-08-02 14:26:05 -0600

    Door

---
+++ Drawn to Death/Assets/Scripts/MiscScripts/Door.cs
@@ -0,0 +1,32 @@
+﻿using EasyButtons;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    [SerializeField] private Animator animator;
+    [SerializeField] Collider2D doorCollider;
+    [SerializeField] private float openDelay = 0.5f;
+    private StateTimer timer;
+
+    public void Start()
+    {
+        timer = new StateTimer(new float[] { openDelay });
+    }
+
+    [Button]
+    public void OpenDoor()
+    {
+        animator?.SetTrigger("Open");
+        timer.Start(() => doorCollider.enabled = false);
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            OpenDoor();
+        }
+    }
+}
