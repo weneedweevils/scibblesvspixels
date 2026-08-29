eaf11120205b47cc3b664ee142170d17c1c353ce
Derek Frevel <derekf711@gmail.com>
2026-08-29 16:09:24 -0600

    Added fade animation to shop

--- Drawn to Death/Assets/Scripts/Upgrade System/UpgradeManager.cs
+++ Drawn to Death/Assets/Scripts/Upgrade System/UpgradeManager.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
+using System;
+using EasyButtons;
 
 public class UpgradeManager : Singleton<UpgradeManager>, IDataPersistence
 {
@@ -12,14 +15,17 @@ public class UpgradeManager : Singleton<UpgradeManager>, IDataPersistence
     public TextMeshProUGUI soulCounter;
     public Soul soulBlueprint;
     [Space(10)] public UpgradeMap[] upgrades;
-    [Space(10)] [TextArea] public string defaultTextboxContent;
-    
+    [Space(10)][TextArea] public string defaultTextboxContent;
+
 
     [Header("UI")]
     public TMPro.TextMeshProUGUI currencyCounter;
     public TMPro.TextMeshProUGUI textbox;
     public TMP_FontAsset basicFont;
     public TMP_FontAsset fancyFont;
+    public GameObject ShopUI;
+    public Image screenFade;
+    public float fadeDuration = 1f;
     public float fontSize;
 
     public void Update()
@@ -37,7 +43,7 @@ public class UpgradeManager : Singleton<UpgradeManager>, IDataPersistence
             upgrades[i].option.Init(i);
         }
     }
-    
+
     public void ApplyUpgrades()
     {
         //Apply the effect for each upgrade that exists
@@ -108,9 +114,94 @@ public class UpgradeManager : Singleton<UpgradeManager>, IDataPersistence
             soul.SetValue(value);
         }
     }
+
+    [Button]
+    public static void OpenShop()
+    {
+        //Freeze movement
+        PlayerMovement.instance.timelinePlaying = true;
+
+        // Stop existing coroutines
+        instance.StopAllCoroutines();
+
+        // Change action map to UI
+        CustomInput.instance.playerInput.SwitchCurrentActionMap("UI");
+
+        // Start opening sequence
+        instance.StartCoroutine(instance.FadeController
+        (
+            instance.fadeDuration,
+            new Color(0, 0, 0, 1),
+            () =>
+            {
+                // On complete -> activeate shop UI and revert the fade effect
+                instance.ShopUI.SetActive(true);
+                instance.StartCoroutine((instance.FadeController(instance.fadeDuration, new Color(0, 0, 0, 0))));
+                
+            }
+         ));
+    }
+
+    /// <summary>
+    /// Closes the shop UI with a smooth fade effect
+    /// </summary>
+    [Button]
+    public static void CloseShop()
+    {
+        // Stop existing coroutines
+        instance.StopAllCoroutines();
+
+        // Change action map to player
+        CustomInput.instance.playerInput.SwitchCurrentActionMap("Player");
+
+        // Start closing sequence
+        instance.StartCoroutine(instance.FadeController
+        (
+            instance.fadeDuration,
+            new Color(0, 0, 0, 1),
+            () =>
+            {
+                // On complete -> close shop UI and revert the fade effect and unfreeze movement
+                instance.ShopUI.SetActive(false);
+                instance.StartCoroutine((instance.FadeController
+                (
+                    instance.fadeDuration, 
+                    new Color(0, 0, 0, 0), 
+                    () => PlayerMovement.instance.timelinePlaying = false
+                )));
+                
+            }
+         ));
+    }
+
+    /// <summary>
+    /// Control fade effect related to the shop
+    /// </summary>
+    private IEnumerator FadeController(float fadeDuration, Color endColor, Action callback = null)
+    {
+        // Save start color
+        Color startColor = screenFade.color;
+        float elapsedTime = 0f;
+
+        // For the duration of the fade
+        while (elapsedTime < fadeDuration)
+        {
+            elapsedTime += Time.deltaTime;
+
+            // Interpolate between start->end colors proportional to ratio of elapsed time to total duration
+            float t = Mathf.Clamp01(elapsedTime / fadeDuration);
+            screenFade.color = Color.Lerp(startColor, endColor, t);
+
+            yield return null;
+        }
+
+        // Effect ended -> invoke the callback (optional)
+        screenFade.color = endColor;
+        callback?.Invoke();
+    }
 }
 
-[System.Serializable]
+    [System.Serializable]
 public struct UpgradeMap
 {
     public Upgrade upgrade;
